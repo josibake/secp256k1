a9326bdd7adacc99ea3e935d59d44f14be4fbbee
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2023-12-23 16:28:18 +0100

    doc: add module description for silentpayments

--- include/secp256k1_silentpayments.h
+++ include/secp256k1_silentpayments.h
@@ -7,11 +7,22 @@
 extern "C" {
 #endif
 
-/* TODO: add module description */
-
-/* TODO: add function API for sender side. */
-
-/* TODO: add function API for receiver side. */
+/* This module provides an implementation for Silent Payments, as specified in BIP352.
+ * This particularly involves the creation of input tweak data by summing up private
+ * or public keys and the derivation of a shared secret using Elliptic Curve Diffie-Hellman.
+ * Combined are either:
+ *   - spender's private keys and receiver's public key (a * B, sender side)
+ *   - spender's public keys and receiver's private key (A * b, receiver side)
+ * With this result, the necessary key material for ultimately creating/scanning
+ * or spending Silent Payment outputs can be determined.
+ *
+ * Note that this module is _not_ a full implementation of BIP352, as it
+ * inherently doesn't deal with higher-level concepts like addresses, output
+ * script types or transactions. The intent is to provide a module for abstracting away
+ * the elliptic-curve operations required for the protocol. For any wallet software already
+ * using libsecp256k1, this API should provide all the functions needed for a Silent Payments
+ * implementation without requiring any further elliptic-curve operations from the wallet.
+ */
 
 #ifdef __cplusplus
 }
